f2cf958c24be424f95d286123438703ae93b650d
Aye Mya Thu <ayemyathu@Ayes-MacBook-Air.local>
2017-10-20 03:36:25 +0900

    save with unique id

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,14 +9,17 @@ import { storage, initializeApp } from 'firebase';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  picdata: any;
+  picurl: any;
+  mypicref: any;
 
   constructor(public navCtrl: NavController, private camera: Camera) {
     initializeApp(config);
+    this.mypicref = storage().ref('/');
   }
 
-  //take photo and save to firebase storage but the last photo
-  async takePhoto() {
-    try{
+  //take photo save to storage and show in img
+  takePhoto(){
     let options: CameraOptions = {
       quality: 100,
       encodingType: this.camera.EncodingType.JPEG,
@@ -26,14 +29,47 @@ export class HomePage {
       correctOrientation: true
     }
 
-    const result = await this.camera.getPicture(options);
-    const image = 'data:image/jpeg;base64,' + result;
-    const picture = storage().ref('pictures');
-    picture.putString(image,'data_url');
-
-  } catch(err){
-    console.log(err);
-  }
+    this.camera.getPicture(options).then((imagedata)=>{
+      this.picdata = imagedata;
+      this.upload();
+    })
   }
 
+  upload(){
+    this.mypicref.child(this.uid()).putString(this.picdata, 'base64',{contentType:'image/png'})
+    .then((savepic)=>{
+      this.picurl = savepic.downloadURL;
+    })
+  }
+
+  uid(){
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx'.replace(/[xy]/g, function (c){
+      var r = (d + Math.random() * 16) % 16 | 0;
+      d = Math.floor(d / 16);
+      return ( c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+    return uuid;
+  }
+
+  /* //take photo and save to firebase storage but the last photo
+  async takePhoto() {
+    try{
+      let options: CameraOptions = {
+        quality: 100,
+        encodingType: this.camera.EncodingType.JPEG,
+        destinationType: this.camera.DestinationType.DATA_URL,
+        sourceType: this.camera.PictureSourceType.CAMERA,
+        mediaType: this.camera.MediaType.PICTURE,
+        correctOrientation: true
+      }
+      const result = await this.camera.getPicture(options);
+      const image = 'data:image/jpeg;base64,' + result;
+      const picture = storage().ref('pictures');
+      picture.putString(image,'data_url');
+    
+    } catch(err){
+      console.log(err);
+    }
+  }  */
 }
