f82ecc4c122e0ed5b61f6c86c32cb149fcebef98
Aye Mya Thu <ayemyathu@Ayes-MacBook-Air.local>
2017-10-20 00:57:22 +0900

    take photo save to storage

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,8 @@
+export const config = {
+    apiKey: "AIzaSyAe8XNppu0JT9Aldh_3PWE457BnOejiJGY",
+    authDomain: "ornate-time-546.firebaseapp.com",
+    databaseURL: "https://ornate-time-546.firebaseio.com",
+    projectId: "ornate-time-546",
+    storageBucket: "ornate-time-546.appspot.com",
+    messagingSenderId: "509492356502"
+  };
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
+import { Camera, CameraOptions } from '@ionic-native/camera';
+import { config } from './../../app/firebase.config';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { storage, initializeApp } from 'firebase';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,30 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private camera: Camera) {
+    initializeApp(config);
+  }
 
+  //take photo and save to firebase storage but the last photo
+  async takePhoto() {
+    try{
+    let options: CameraOptions = {
+      quality: 100,
+      encodingType: this.camera.EncodingType.JPEG,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: this.camera.PictureSourceType.CAMERA,
+      mediaType: this.camera.MediaType.PICTURE,
+      correctOrientation: true
+    }
+
+    const result = await this.camera.getPicture(options);
+    const image = 'data:image/jpeg;base64,' + result;
+    const picture = storage().ref('pictures');
+    picture.putString(image,'data_url');
+
+  } catch(err){
+    console.log(err);
+  }
   }
 
 }
